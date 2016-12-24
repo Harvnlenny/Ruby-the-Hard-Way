513740ed469945e27f2c4749661c61d5e93e444e
Harvnlenny <aaron@sparxoo.com>
2016-12-23 21:03:14 -0500

    added ex18

--- ex17.rb
+++ ex17.rb
@@ -7,3 +7,15 @@ in_file = open(from_file)
 indata = in_file.read
 
 puts "The input file is #{indata.length} bytes long"
+
+puts "Does the output file exist? #{File.exist?(to_file)}"
+puts "Ready, hit RETURN to continue, CTRL-C to abort."
+$stdin.gets
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+puts "Alright, all done."
+
+out_file.close
+in_file.close
